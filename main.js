296b7d78eef3986f0ae081560f0da5c10bbb9f84
Parul Upadhyaya <pupadhy2@ncsu.edu>
2015-03-26 18:28:51 -0400

    adding hw

--- main.js
+++ main.js
@@ -4,55 +4,91 @@ var express = require('express')
 var fs      = require('fs')
 var app = express()
 // REDIS
-//var client = redis.createClient(6379, '127.0.0.1', {})
+var redis = require('redis')
+var client = redis.createClient(6379, '127.0.0.1', {})
 
-///////////// WEB ROUTES
+
+///////////// WEB ROUTES 
+
 
 // Add hook to make it easier to get all visited URLS.
-app.use(function(req, res, next) 
+app.get('/recent', function(req, res, next) 
 {
-	console.log(req.method, req.url);
-
-	// ... INSERT HERE.
-
-	next(); // Passing the request to the next handler in the stack.
+	 //  console.log(req.method, req.url);
+       client.lpush('urls',req.url);
+	   
+	   client.ltrim('urls', 0, 4);
+	  
+	   client.lrange('urls',0, 4, function(err,value){ console.log(value);});
+	   client.lrange('urls',0, 4, function(err,value){ res.send(value);});
+       	   
+	  // next(); // Passing the request to the next handler in the stack.   
+	        
 });
 
-
-// app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
+var items = [];
+ app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
 //    console.log(req.body) // form fields
 //    console.log(req.files) // form files
+    
+    if( req.files.image )
+    {
+ 	   fs.readFile( req.files.image.path, function (err, data) {
+ 	  		if (err) throw err;
+ 	  		var img = new Buffer(data).toString('base64');
+			items.push(img);
+ 	  	//	console.log(img);
+ 		});
+ 	}
+   res.status(204).end()
+ }]);
 
-//    if( req.files.image )
-//    {
-// 	   fs.readFile( req.files.image.path, function (err, data) {
-// 	  		if (err) throw err;
-// 	  		var img = new Buffer(data).toString('base64');
-// 	  		console.log(img);
-// 		});
-// 	}
-
-//    res.status(204).end()
-// }]);
-
-// app.get('/meow', function(req, res) {
-// 	{
-// 		if (err) throw err
-// 		res.writeHead(200, {'content-type':'text/html'});
-// 		items.forEach(function (imagedata) 
-// 		{
-//    		res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
-// 		});
-//    	res.end();
-// 	}
-// })
+app.get('/meow', function(req, res) {
+ 	{
+	    client.lpush('urls',req.url);
+ 	//	if (err) throw err
+ 		res.writeHead(200, {'content-type':'text/html'});
+ 		items.forEach(function (imagedata) 
+ 		{
+    		temp_Image = imagedata;
+		//	console.log(imagedata);
+ 		});
+		res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+temp_Image+"'/>");
+		items.pop();
+    	res.end();
+ 	}
+ })
+ 
 
 // HTTP SERVER
-// var server = app.listen(3000, function () {
+ var server = app.listen(3000, function () {
 
-//   var host = server.address().address
-//   var port = server.address().port
+   var host = server.address().address
+   var port = server.address().port
 
-//   console.log('Example app listening at http://%s:%s', host, port)
-// })
+   console.log('Example app listening at http://%s:%s', host, port)
+ })
+ 
+ // HTTP SERVER
+ var server1 = app.listen(3001, function () {
+
+   var host = server1.address().address
+   var port = server1.address().port
+
+   console.log('Example app listening at http://%s:%s', host, port)
+ })
+ 
+ 
+ app.get('/get', function(req, res) {
+  client.get("key", function(err,value){ res.send(value) });
+  //res.send('hello world')
+    client.lpush('urls',req.url);
+ //  res.send(client.get("key"));
+})
+
+ app.get('/set', function(req,res) {
+  client.lpush('urls',req.url);
+  client.set("key","this message will self-destruct in 10 seconds");
+  client.expire("key",10);
+})
 
---
+++ proxy.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var fs      = require('fs')
+var app = express()
+var redis = require('redis')
+var client = redis.createClient(6379, '127.0.0.1', {})
+var http=require('http');
+
+var proxyServer = http.createServer(function(req,res){
+	res.writeHead(301, {Location: 'http://localhost:3000'+ req.url});
+       //res.redirect("http://localhost:3000" + req.url)
+       res.end();
+})
+proxyServer.listen(3002);
+
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log('Example app listening at http://%s:%s', host, port)
+ })
